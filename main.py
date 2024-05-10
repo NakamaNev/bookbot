878a4fba8c07228da4329a4621d73dfb10c9bfe4
NakamaNev <jamari_mitchell@hotmail.com>
2024-05-10 07:29:38 -0700

    added character count

--- main.py
+++ main.py
@@ -3,11 +3,22 @@ def main():
         file_contents = f.read()
     word_count = words_count(file_contents)
     print(file_contents)
-    print (f"word count= {word_count}")
+    print (f"word count = {word_count}")
+    print (f"character count = {char_count(file_contents)}")
 
 def words_count(file_contents):
     words= file_contents.split()
     return len(words)
+
+def char_count(file_contents):
+    char_counter= {}
+    for char in file_contents.lower():
+        if char.isalnum():
+            if char in char_counter:
+                char_counter[char] += 1
+            else:
+                char_counter[char] = 1
+    return char_counter
 
 
 
